6ddad7e9c22d19b4bf2afa7c708fd6a5f43e2b2c
Wolmin <lamonos123@gmail.com>
2024-09-02 10:10:02 +0200

    Correct wording for sending LYX

--- ui/address/contract/methodForm/ContractMethodForm.tsx
+++ ui/address/contract/methodForm/ContractMethodForm.tsx
@@ -73,7 +73,7 @@ const ContractMethodForm = <T extends SmartContractMethod>({ data, onSubmit, res
     return [
       ...('inputs' in data ? data.inputs : []),
       ...('stateMutability' in data && data.stateMutability === 'payable' ? [ {
-        name: `Send native ${ config.chain.currency.symbol || 'coin' }`,
+        name: `Send native ${ config.chain.currency.symbol || 'coin' } in WEI`,
         type: 'uint256' as const,
         internalType: 'uint256' as const,
         fieldType: 'native_coin' as const,
