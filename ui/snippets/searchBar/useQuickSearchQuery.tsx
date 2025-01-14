1eb82b8e0ee9377276a3efc0366fd5a096c26428
Wolmin <lamonos123@gmail.com>
2025-01-14 11:57:40 +0100

    Merge two queries together
    Signed-off-by: Wolmin <lamonos123@gmail.com>

--- ui/snippets/searchBar/useQuickSearchQuery.tsx
+++ ui/snippets/searchBar/useQuickSearchQuery.tsx
@@ -1,6 +1,7 @@
-// import { useQuery } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+
+import type { SearchResultItem } from '../../../types/api/search';
 
 import { isBech32Address, fromBech32Address } from 'lib/address/bech32';
 import useApiQuery from 'lib/api/useApiQuery';
@@ -13,14 +14,10 @@ export default function useQuickSearchQuery() {
 
   const debouncedSearchTerm = useDebounce(searchTerm, 300);
 
-  const query = useApiQuery('quick_search', {
+  const quickSearchQuery = useApiQuery('quick_search', {
     queryParams: { q: isBech32Address(debouncedSearchTerm) ? fromBech32Address(debouncedSearchTerm) : debouncedSearchTerm },
     queryOptions: { enabled: debouncedSearchTerm.trim().length > 0 },
   });
-  // const query = useApiQuery('quick_search', {
-  //   queryParams: { q: debouncedSearchTerm },
-  //   queryOptions: { enabled: debouncedSearchTerm.trim().length > 0 },
-  // });
 
   const redirectCheckQuery = useApiQuery('search_check_redirect', {
     // on pages with regular search bar we check redirect on every search term change
@@ -29,9 +26,19 @@ export default function useQuickSearchQuery() {
     queryOptions: { enabled: Boolean(debouncedSearchTerm) },
   });
 
-  const query = useUniversalProfileQuery('universal_profile', {
+  const upQuery = useUniversalProfileQuery('universal_profile', {
     queryParams: { q: debouncedSearchTerm },
     queryOptions: { enabled: debouncedSearchTerm.trim().length > 0 },
+  });
+
+  const query = useQuery({
+    queryKey: [ 'merged_query', quickSearchQuery, upQuery ],
+    queryFn: () => {
+      const q1 = quickSearchQuery.data as Array<SearchResultItem>;
+      const q2 = upQuery.data as Array<SearchResultItem>;
+
+      return [ ...q1, ...q2 ];
+    },
   });
 
   return React.useMemo(() => ({
